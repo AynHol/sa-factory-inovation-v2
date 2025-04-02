f01188747ae057adbcfb31eef7e81e77fb8e3b14
WESLEY ALESSANDRO DARÉ ANTUNES <wesley_a_antunes@estudante.sesisenai.org.br>
2025-04-01 21:57:28 -0300

    continuação

--- src/app/(public-routes)/login/page.tsx
+++ src/app/(public-routes)/login/page.tsx
@@ -1,34 +1,34 @@
 import Image from "next/image";
-import "./styles.css";
+import styles from "./styles.module.css";
 import carFactory from "@/assets/factory.jpg";
 import Link from "next/link";
 import Password from "@/components/Password";
 
 export default function Login() {
     return (
-        <div className="body">
-            <div className="main">
-                <div className="login">
-                    <div className="name-p">
-                        <div className="name">
+        <div className={styles.body}>
+            <div className={styles.main}>
+                <div className={styles.login}>
+                    <div className={styles.name_p}>
+                        <div className={styles.name}>
                             <h1>Factory Inovation</h1>
                             <p>v2</p>
                         </div>
                         <p>Por favor entre com as suas informações</p>
                     </div>
-                    <div className="inputs">
+                    <div className={styles.inputs}>
                         <input type="text" placeholder="Email" />
                         <Password placeholder="Password" />
                     </div>
-                    <button className="login-button">Log in</button>
-                    <div className="cadastro">
+                    <button className={styles.login_button}>Log in</button>
+                    <div className={styles.cadastro}>
                         <p>Não tem uma conta?</p>
-                        <Link href="/signup" className="link">
+                        <Link href="/signup" className={styles.link}>
                             Cadastrar-se
                         </Link>
                     </div>
                 </div>
-                <Image src={carFactory} alt="Car Factory" className="car-factory" />
+                <Image src={carFactory} alt="Car Factory" className={styles.car_factory} />
             </div>
         </div>
     );
